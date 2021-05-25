e61b78d4b6509b6007a062fbb2128cf3b3ca13e9
Filipe Marques <filipe.s.marques@tecnico.ulisboa.pt>
2021-05-25 13:02:10 +0100

    totals

--- wasp/tests/sv-comp/results/table_results.py
+++ wasp/tests/sv-comp/results/table_results.py
@@ -1,92 +1,120 @@
-import sys, csv
+import sys, csv, os, glob
 
-def main(f):
+def main(f, m):
     with open(f, 'r') as csvFile:
         csvReader = csv.reader(csvFile)
         data = list(csvReader)
 
-    total = len(data)
-    completeTrueNeg = len(list(filter( \
+    m['total'] += len(data) - 1
+    m['completeTrueNeg'] += len(list(filter( \
             lambda l: (l[1] == 'OK') and (l[2] == 'OK') and (l[3] == 'True'), \
             data)))
 
-    incompleteTrueNeg = len(list(filter( \
+    m['incompleteTrueNeg'] += len(list(filter( \
             lambda l: (l[1] == 'OK') and (l[2] == 'OK') and (l[3] == 'False'), \
             data)))
 
-    truePos = len(list(filter( \
+    m['truePos'] += len(list(filter( \
             lambda l: (l[1] == 'NOK') and (l[2] == 'NOK'), \
             data)))
 
-    timeoutsTrueNeg = len(list(filter( \
+    m['timeoutsTrueNeg'] += len(list(filter( \
             lambda l: (l[1] == 'TIMEOUT') and (l[2] == 'OK'), \
             data)))
 
-    crashFalsePos = len(list(filter( \
+    m['crashFalsePos'] += len(list(filter( \
             lambda l: (l[1] == 'CRASH') and (l[2] == 'OK'), \
             data)))
 
-    completeFalsePos = len(list(filter( \
+    m['completeFalsePos'] += len(list(filter( \
             lambda l: (l[1] == 'NOK') and (l[2] == 'OK') and (l[3] == 'True'), \
             data)))
 
-    incompleteFalsePos = len(list(filter( \
+    m['incompleteFalsePos'] += len(list(filter( \
             lambda l: (l[1] == 'NOK') and (l[2] == 'OK') and (l[3] == 'False'), \
             data)))
 
 
-    completeFalseNeg = len(list(filter( \
+    m['completeFalseNeg'] += len(list(filter( \
             lambda l: (l[1] == 'OK') and (l[2] == 'NOK') and (l[3] == 'True'), \
             data)))
 
-    incompleteFalseNeg = len(list(filter( \
+    m['incompleteFalseNeg'] += len(list(filter( \
             lambda l: (l[1] == 'OK') and (l[2] == 'NOK') and (l[3] == 'False'), \
             data)))
 
-    timeoutFalseNeg = len(list(filter( \
+    m['timeoutFalseNeg'] += len(list(filter( \
             lambda l: (l[1] == 'TIMEOUT') and (l[2] == 'NOK'), \
             data)))
 
-    crashTruePos = len(list(filter( \
+    m['crashTruePos'] += len(list(filter( \
             lambda l: (l[1] == 'CRASH') and (l[2] == 'NOK'), \
             data)))
 
-    totalTruePos = truePos + crashTruePos
-    totalTrueNeg = incompleteTrueNeg + completeTrueNeg + timeoutsTrueNeg
-    totalFalsePos = incompleteFalsePos + completeFalsePos + crashFalsePos
-    totalFalseNeg = incompleteFalseNeg + completeFalseNeg + timeoutFalseNeg
+    m['totalTruePos'] = m['truePos']+m['crashTruePos'] 
+    m['totalTrueNeg'] = m['incompleteTrueNeg']+ m['completeTrueNeg']+m['timeoutsTrueNeg'] 
+    m['totalFalsePos'] = m['incompleteFalsePos']+ m['completeFalsePos']+m['crashFalsePos'] 
+    m['totalFalseNeg'] = m['incompleteFalseNeg']+ m['completeFalseNeg']+m['timeoutFalseNeg'] 
 
-    totalOk = totalTrueNeg + totalFalsePos
-    totalNok = totalTruePos + totalFalseNeg
+    m['totalOk'] = m['totalTrueNeg']+m['totalFalsePos'] 
+    m['totalNok'] = m['totalTruePos']+m['totalFalseNeg'] 
 
-    timeout = len(list(filter(lambda l: (l[1] == 'TIMEOUT'), data)))
-    crash   = len(list(filter(lambda l: (l[1] == 'CRASH'), data)))
+    m['timeout'] += len(list(filter(lambda l: (l[1] == 'TIMEOUT'), data)))
+    m['crash'] += len(list(filter(lambda l: (l[1] == 'CRASH'), data)))
 
+    return m
 
+if __name__ == '__main__':
+    m = dict( \
+            truePos = 0, \
+            crashTruePos = 0, \
+            totalTruePos = 0, \
+            incompleteTrueNeg = 0, \
+            completeTrueNeg = 0, \
+            timeoutsTrueNeg = 0, \
+            totalTrueNeg = 0, \
+            incompleteFalsePos = 0, \
+            completeFalsePos = 0, \
+            crashFalsePos = 0, \
+            totalFalsePos = 0, \
+            incompleteFalseNeg = 0, \
+            completeFalseNeg = 0, \
+            timeoutFalseNeg = 0, \
+            totalFalseNeg = 0, \
+            totalOk = 0, \
+            totalNok = 0, \
+            timeout = 0, \
+            crash = 0, \
+            total = 0)
+    src = []
+    if (os.path.isdir(sys.argv[1])):
+        src = glob.glob(sys.argv[1] + "/*.csv")
+    else:
+        src = [sys.argv[1]]
+
+    for path in src:
+        m = main(path, m)
 
     print(f'''
 | Prop | Count | percent |
 |:----:|:------|:--|
-|complete   (T-)|{completeTrueNeg}| {(completeTrueNeg/totalOk) * 100} |
-|incomplete (T-)|{incompleteTrueNeg}|{(incompleteTrueNeg/totalOk) * 100} |
-|timeout    (T-)|{timeoutsTrueNeg}|{(timeoutsTrueNeg/totalOk) * 100} |
-|           (T+)|{truePos}|{(truePos/totalNok) * 100} |
-|crash      (T+)|{crashTruePos}|{(crashTruePos/totalNok) * 100} |
-|complete   (F-)|{completeFalseNeg}|{(completeFalseNeg/totalNok) * 100} |
-|incomplete (F-)|{incompleteFalseNeg}|{(incompleteFalseNeg/totalNok) * 100} |
-|timeout    (F-)|{timeoutFalseNeg}|{(timeoutFalseNeg/totalNok) * 100} |
-|complete   (F+)|{completeFalsePos}|{(completeFalsePos/totalOk) * 100} |
-|incomplete (F+)|{incompleteFalsePos}|{(incompleteFalsePos/totalOk) * 100} |
-|crash      (F+)|{crashFalsePos}|{(crashFalsePos/totalOk) * 100} |
-|timeout        |{timeout}|
-|crash          |{crash}|
-|Total          |{total-1}|
+|complete   (T-)|{m['completeTrueNeg']}| {(m['completeTrueNeg']/m['totalOk']) * 100} |
+|incomplete (T-)|{m['incompleteTrueNeg']}|{(m['incompleteTrueNeg']/m['totalOk']) * 100} |
+|timeout    (T-)|{m['timeoutsTrueNeg']}|{(m['timeoutsTrueNeg']/m['totalOk']) * 100} |
+|           (T+)|{m['truePos']}|{(m['truePos']/m['totalNok']) * 100} |
+|crash      (T+)|{m['crashTruePos']}|{(m['crashTruePos']/m['totalNok']) * 100} |
+|complete   (F-)|{m['completeFalseNeg']}|{(m['completeFalseNeg']/m['totalNok']) * 100} |
+|incomplete (F-)|{m['incompleteFalseNeg']}|{(m['incompleteFalseNeg']/m['totalNok']) * 100} |
+|timeout    (F-)|{m['timeoutFalseNeg']}|{(m['timeoutFalseNeg']/m['totalNok']) * 100} |
+|complete   (F+)|{m['completeFalsePos']}|{(m['completeFalsePos']/m['totalOk']) * 100} |
+|incomplete (F+)|{m['incompleteFalsePos']}|{(m['incompleteFalsePos']/m['totalOk']) * 100} |
+|crash      (F+)|{m['crashFalsePos']}|{(m['crashFalsePos']/m['totalOk']) * 100} |
+|timeout        |{m['timeout']}| |
+|crash          |{m['crash']}| |
+|Total          |{m['total']}| |
 
 | Actual Property\Reported Property | holds | does not hold |
 |------------------------------------|-------|---------------|
-| holds | {completeTrueNeg}+{incompleteTrueNeg}+{timeoutsTrueNeg}={totalTrueNeg} | {completeFalsePos}+{incompleteFalsePos}+{crashFalsePos}={totalFalsePos} |
-| does not hold | {completeFalseNeg}+{incompleteFalseNeg}+{timeoutFalseNeg}={totalFalseNeg} | {truePos}+{crashTruePos}={totalTruePos} |
+| holds | {m['completeTrueNeg']}+{m['incompleteTrueNeg']}+{m['timeoutsTrueNeg']}={m['totalTrueNeg']}/{m['totalOk']} | {m['completeFalsePos']}+{m['incompleteFalsePos']}+{m['crashFalsePos']}={m['totalFalsePos']} |
+| does not hold | {m['completeFalseNeg']}+{m['incompleteFalseNeg']}+{m['timeoutFalseNeg']}={m['totalFalseNeg']} | {m['truePos']}+{m['crashTruePos']}={m['totalTruePos']}/{m['totalNok']} |
 ''')
-
-if __name__ == '__main__':
-    main(sys.argv[1])
