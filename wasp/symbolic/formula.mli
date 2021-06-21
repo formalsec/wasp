0705c5a9efb600f27f78caeb4287039140d0f296
Filipe Marques <filipe.s.marques@tecnico.ulisboa.pt>
2021-06-21 19:39:24 +0100

    interfaces

---
+++ wasp/symbolic/formula.mli
@@ -0,0 +1,18 @@
+open Symvalue 
+
+type formula =
+  | True
+  | False
+  | Not of formula
+  | And of formula * formula
+  | Or  of formula * formula
+  | Relop of sym_expr
+
+type t = formula
+
+val negate : formula -> formula
+val conjuct : formula list -> formula
+val length : formula -> int
+
+val to_formula : sym_expr list -> formula
+val to_string : formula -> string
