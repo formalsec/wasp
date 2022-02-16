97677869f0168dff2786e53502d8edad2a7486cc
João Borges <joao.p.l.borges@tecnico.ulisboa.pt>
2022-02-16 13:15:42 +0000

    style: indentation, 2 spaces instead of tabs

--- wasp/symbolic/sf32.ml
+++ wasp/symbolic/sf32.ml
@@ -16,54 +16,54 @@ let neg_relop (op : relop) : relop =
 
 (*  String representation of an f32 binary operation  *)
 let string_of_binop (op : binop) : string =
-	match op with
-	| F32Add -> "F32Add"
-	| F32Sub -> "F32Sub"
-	| F32Mul -> "F32Mul"
-	| F32Div -> "F32Div"
-	| F32Min -> "F32Min"
-	| F32Max -> "F32Max"
+  match op with
+  | F32Add -> "F32Add"
+  | F32Sub -> "F32Sub"
+  | F32Mul -> "F32Mul"
+  | F32Div -> "F32Div"
+  | F32Min -> "F32Min"
+  | F32Max -> "F32Max"
 
 let pp_string_of_binop (op : binop) : string =
-	match op with
-	| F32Add -> "+"
-	| F32Sub -> "-"
-	| F32Mul -> "*"
-	| F32Div -> "/"
-	| F32Min -> "F32Min"
-	| F32Max -> "F32Max"
+  match op with
+  | F32Add -> "+"
+  | F32Sub -> "-"
+  | F32Mul -> "*"
+  | F32Div -> "/"
+  | F32Min -> "F32Min"
+  | F32Max -> "F32Max"
 
 (*  String representation of an f32 unary operation  *)
 let string_of_unop (op : unop) : string =
-	match op with
-	| F32Neg -> "F32Neg"
+  match op with
+  | F32Neg -> "F32Neg"
   | F32Abs -> "F32Abs"
   | F32Sqrt -> "F32Sqrt"
 
 let pp_string_of_unop (op : unop) : string =
-	match op with
-	| F32Neg -> "-"
+  match op with
+  | F32Neg -> "-"
   | F32Abs -> "F32Abs"
   | F32Sqrt -> "F32Sqrt"
 
 (*  String representation of an f32 relative operation  *)
 let string_of_relop (op : relop) : string =
-	match op with
-	| F32Eq -> "F32Eq"
-	| F32Ne -> "F32Ne"
-	| F32Lt -> "F32Lt"
-	| F32Gt -> "F32Gt"
-	| F32Le -> "F32Le"
-	| F32Ge -> "F32Ge"
+  match op with
+  | F32Eq -> "F32Eq"
+  | F32Ne -> "F32Ne"
+  | F32Lt -> "F32Lt"
+  | F32Gt -> "F32Gt"
+  | F32Le -> "F32Le"
+  | F32Ge -> "F32Ge"
 
 let pp_string_of_relop (op : relop) : string =
-	match op with
-	| F32Eq -> "=="
-	| F32Ne -> "!="
-	| F32Lt -> "<"
-	| F32Gt -> ">"
-	| F32Le -> "<="
-	| F32Ge -> ">="
+  match op with
+  | F32Eq -> "=="
+  | F32Ne -> "!="
+  | F32Lt -> "<"
+  | F32Gt -> ">"
+  | F32Le -> "<="
+  | F32Ge -> ">="
 
 let string_of_cvtop (op : cvtop) : string =
   match op with
