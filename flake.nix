1979a2fee685fe188db70f35eefd51677fab27a4
João Borges <joao.p.l.borges@tecnico.ulisboa.pt>
2022-07-03 15:33:53 +0100

    chore: add nix flake to handle dependencies
    (these 2 files can just be ignored)

---
+++ flake.nix
@@ -0,0 +1,37 @@
+{
+  description = "nix flake for building wasp";
+
+  inputs = {
+    nixpkgs.url = "github:NixOS/nixpkgs";
+    flake-utils.url = "github:numtide/flake-utils";
+  };
+
+  outputs = { nixpkgs, flake-utils, ... }:
+    flake-utils.lib.eachDefaultSystem (system:
+      let
+        pkgs = import nixpkgs { inherit system; };
+      in {
+        # TODO: make this actually pure instead of using an environment
+        devShell = pkgs.mkShell {
+          nativeBuildInputs = with pkgs; [
+            gnumake
+            opam
+            python27Full
+            python39Full
+            python39Packages.pycparser
+            libcxx
+            gmp
+            clang_10
+            llvmPackages_10.bintools-unwrapped
+            wabt
+          ];
+          shellHook = ''
+          export LD_LIBRARY_PATH="$HOME/.opam/4.08.1/lib/z3/:$LD_LIBRARY_PATH"
+          export PATH="$PATH:$PWD/wasp"
+          # export PATH="$PATH:$PWD/wasp-c/bin"
+          eval $(opam env)
+          '';
+        };
+      }
+      );
+}
