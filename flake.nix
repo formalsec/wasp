4b43102b9891e0334a96b3a89df339006b58d5de
João Borges <joao.p.l.borges@tecnico.ulisboa.pt>
2023-01-05 11:33:50 +0000

    refactor: Format flake.nix

--- flake.nix
+++ flake.nix
@@ -6,10 +6,24 @@
     flake-utils.url = "github:numtide/flake-utils";
   };
 
-  outputs = { nixpkgs, flake-utils, ... }:
-    flake-utils.lib.eachDefaultSystem (system:
-      let
-        pkgs = import nixpkgs { inherit system; };
+  outputs = {
+    nixpkgs,
+    flake-utils,
+    ...
+  }:
+    flake-utils.lib.eachDefaultSystem (
+      system: let
+        pkgs = import nixpkgs {inherit system;};
+
+        my_python3_packages = p: [
+          # wasp-c
+          p.pycparser
+          # data wrangling
+          p.pandas
+          p.numpy
+        ];
+
+        my_python3 = pkgs.python39.withPackages my_python3_packages;
       in {
         # TODO: make this actually pure instead of using an environment
         devShell = pkgs.mkShell {
@@ -17,9 +31,7 @@
             gnumake
             opam
             python27Full
-            python39Full
-            python39Packages.pycparser
-            python39Packages.lxml
+            my_python3
             libcxx
             gmp
             clang_10
@@ -27,12 +39,12 @@
             wabt
           ];
           shellHook = ''
-          export LD_LIBRARY_PATH="$HOME/.opam/wasp/lib/z3/:$LD_LIBRARY_PATH"
-          export PATH="$PWD/wasp/_build/install/default/bin:$PATH"
-          # export PATH="$PATH:$PWD/wasp-c/bin"
-          eval $(opam env)
+            export LD_LIBRARY_PATH="$HOME/.opam/wasp/lib/z3/:$LD_LIBRARY_PATH"
+            export PATH="$PWD/wasp/_build/install/default/bin:$PATH"
+            # export PATH="$PATH:$PWD/wasp-c/bin"
+            eval $(opam env)
           '';
         };
       }
-      );
+    );
 }
