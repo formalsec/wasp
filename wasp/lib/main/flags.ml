8f89beb759d8a91ccaa24a957ff2ef3953fff2fc
Filipe Marques <filipe.s.marques@tecnico.ulisboa.pt>
2023-04-03 15:18:42 +0100

    refactor: remove instruction limit flag

--- wasp/lib/main/flags.ml
+++ wasp/lib/main/flags.ml
@@ -5,7 +5,6 @@ let print_sig = ref false
 let dry = ref false
 let width = ref 80
 let harness = ref true
-let inst_limit = ref (-1)
 let output = ref "output"
 let simplify = ref true
 let timeout = ref 895
