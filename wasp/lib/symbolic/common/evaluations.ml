1acca617ceed61e3462418655a9bdd093e5973c7
Filipe Marques <filipe.s.marques@tecnico.ulisboa.pt>
2023-04-14 12:21:33 +0100

    refactor: use abtract syntax constructors

--- wasp/lib/symbolic/common/evaluations.ml
+++ wasp/lib/symbolic/common/evaluations.ml
@@ -33,123 +33,123 @@ let to_num_type (t : Interpreter.Types.value_type) =
 
 let f32_unop op e =
   match op with
-  | F32Op.Neg -> Unop (F32 Neg, e)
-  | F32Op.Abs -> Unop (F32 Abs, e)
-  | F32Op.Sqrt -> Unop (F32 Sqrt, e)
-  | F32Op.Nearest -> Unop (F32 Nearest, e)
+  | F32Op.Neg -> FloatingPoint.mk_neg e `F32Type
+  | F32Op.Abs -> FloatingPoint.mk_abs e `F32Type
+  | F32Op.Sqrt -> FloatingPoint.mk_sqrt e `F32Type
+  | F32Op.Nearest -> FloatingPoint.mk_nearest e `F32Type
   | F32Op.Ceil -> raise (UnsupportedOp "eval_unop: Ceil")
   | F32Op.Floor -> raise (UnsupportedOp "eval_unop: Floor")
   | F32Op.Trunc -> raise (UnsupportedOp "eval_unop: Trunc")
 
 let f64_unop op e =
   match op with
-  | F64Op.Neg -> Unop (F64 Neg, e)
-  | F64Op.Abs -> Unop (F64 Abs, e)
-  | F64Op.Sqrt -> Unop (F64 Sqrt, e)
-  | F64Op.Nearest -> Unop (F64 Nearest, e)
+  | F64Op.Neg -> FloatingPoint.mk_neg e `F64Type
+  | F64Op.Abs -> FloatingPoint.mk_abs e `F64Type
+  | F64Op.Sqrt -> FloatingPoint.mk_sqrt e `F64Type
+  | F64Op.Nearest -> FloatingPoint.mk_nearest e `F64Type
   | F64Op.Ceil -> raise (UnsupportedOp "eval_unop: Ceil")
   | F64Op.Floor -> raise (UnsupportedOp "eval_unop: Floor")
   | F64Op.Trunc -> raise (UnsupportedOp "eval_unop: Trunc")
 
 let i32_binop op e1 e2 =
   match op with
-  | I32Op.Add -> Binop (I32 Add, e1, e2)
-  | I32Op.And -> Binop (I32 And, e1, e2)
-  | I32Op.Or -> Binop (I32 Or, e1, e2)
-  | I32Op.Sub -> Binop (I32 Sub, e1, e2)
-  | I32Op.DivS -> Binop (I32 DivS, e1, e2)
-  | I32Op.DivU -> Binop (I32 DivU, e1, e2)
-  | I32Op.Xor -> Binop (I32 Xor, e1, e2)
-  | I32Op.Mul -> Binop (I32 Mul, e1, e2)
-  | I32Op.Shl -> Binop (I32 Shl, e1, e2)
-  | I32Op.ShrS -> Binop (I32 ShrS, e1, e2)
-  | I32Op.ShrU -> Binop (I32 ShrU, e1, e2)
-  | I32Op.RemS -> Binop (I32 RemS, e1, e2)
-  | I32Op.RemU -> Binop (I32 RemU, e1, e2)
+  | I32Op.Add -> BitVector.mk_add e1 e2 `I32Type
+  | I32Op.And -> BitVector.mk_and e1 e2 `I32Type
+  | I32Op.Or -> BitVector.mk_or e1 e2 `I32Type
+  | I32Op.Sub -> BitVector.mk_sub e1 e2 `I32Type
+  | I32Op.DivS -> BitVector.mk_div_s e1 e2 `I32Type
+  | I32Op.DivU ->  BitVector.mk_div_u e1 e2 `I32Type
+  | I32Op.Xor -> BitVector.mk_xor e1 e2 `I32Type
+  | I32Op.Mul -> BitVector.mk_mul e1 e2 `I32Type
+  | I32Op.Shl -> BitVector.mk_shl e1 e2 `I32Type
+  | I32Op.ShrS -> BitVector.mk_shr_s e1 e2 `I32Type
+  | I32Op.ShrU -> BitVector.mk_shr_u e1 e2 `I32Type
+  | I32Op.RemS -> BitVector.mk_rem_s e1 e2 `I32Type
+  | I32Op.RemU -> BitVector.mk_rem_u e1 e2 `I32Type
   | I32Op.Rotl -> failwith "eval I32Binop: TODO Rotl"
   | I32Op.Rotr -> failwith "eval I32Binop: TODO Rotr"
 
 let i64_binop op e1 e2 =
   match op with
-  | I64Op.Add -> Binop (I64 Add, e1, e2)
-  | I64Op.And -> Binop (I64 And, e1, e2)
-  | I64Op.Or -> Binop (I64 Or, e1, e2)
-  | I64Op.Sub -> Binop (I64 Sub, e1, e2)
-  | I64Op.DivS -> Binop (I64 DivS, e1, e2)
-  | I64Op.DivU -> Binop (I64 DivU, e1, e2)
-  | I64Op.Xor -> Binop (I64 Xor, e1, e2)
-  | I64Op.Mul -> Binop (I64 Mul, e1, e2)
-  | I64Op.RemS -> Binop (I64 RemS, e1, e2)
-  | I64Op.RemU -> Binop (I64 RemU, e1, e2)
-  | I64Op.Shl -> Binop (I64 Shl, e1, e2)
-  | I64Op.ShrS -> Binop (I64 ShrS, e1, e2)
-  | I64Op.ShrU -> Binop (I64 ShrU, e1, e2)
+  | I64Op.Add -> BitVector.mk_add e1 e2 `I64Type
+  | I64Op.And -> BitVector.mk_and e1 e2 `I64Type
+  | I64Op.Or -> BitVector.mk_or e1 e2 `I64Type
+  | I64Op.Sub -> BitVector.mk_sub e1 e2 `I64Type
+  | I64Op.DivS -> BitVector.mk_div_s e1 e2 `I64Type
+  | I64Op.DivU ->  BitVector.mk_div_u e1 e2 `I64Type
+  | I64Op.Xor -> BitVector.mk_xor e1 e2 `I64Type
+  | I64Op.Mul -> BitVector.mk_mul e1 e2 `I64Type
+  | I64Op.Shl -> BitVector.mk_shl e1 e2 `I64Type
+  | I64Op.ShrS -> BitVector.mk_shr_s e1 e2 `I64Type
+  | I64Op.ShrU -> BitVector.mk_shr_u e1 e2 `I64Type
+  | I64Op.RemS -> BitVector.mk_rem_s e1 e2 `I64Type
+  | I64Op.RemU -> BitVector.mk_rem_u e1 e2 `I64Type
   | I64Op.Rotl -> failwith "eval I64Binop: TODO Rotl"
   | I64Op.Rotr -> failwith "eval I64Binop: TODO Rotr"
 
 let f32_binop op e1 e2 =
   match op with
-  | F32Op.Add -> Binop (F32 Add, e1, e2)
-  | F32Op.Sub -> Binop (F32 Sub, e1, e2)
-  | F32Op.Div -> Binop (F32 Div, e1, e2)
-  | F32Op.Mul -> Binop (F32 Mul, e1, e2)
-  | F32Op.Min -> Binop (F32 Min, e1, e2)
-  | F32Op.Max -> Binop (F32 Max, e1, e2)
+  | F32Op.Add -> FloatingPoint.mk_add e1 e2 `F32Type
+  | F32Op.Sub -> FloatingPoint.mk_sub e1 e2 `F32Type
+  | F32Op.Div -> FloatingPoint.mk_div e1 e2 `F32Type
+  | F32Op.Mul -> FloatingPoint.mk_mul e1 e2 `F32Type
+  | F32Op.Min -> FloatingPoint.mk_min e1 e2 `F32Type
+  | F32Op.Max -> FloatingPoint.mk_max e1 e2 `F32Type
   | F32Op.CopySign -> failwith "eval F32Binop: TODO CopySign"
 
 let f64_binop op e1 e2 =
   match op with
-  | F64Op.Add -> Binop (F64 Add, e1, e2)
-  | F64Op.Sub -> Binop (F64 Sub, e1, e2)
-  | F64Op.Div -> Binop (F64 Div, e1, e2)
-  | F64Op.Mul -> Binop (F64 Mul, e1, e2)
-  | F64Op.Min -> Binop (F64 Min, e1, e2)
-  | F64Op.Max -> Binop (F64 Max, e1, e2)
+  | F64Op.Add -> FloatingPoint.mk_add e1 e2 `F64Type
+  | F64Op.Sub -> FloatingPoint.mk_sub e1 e2 `F64Type
+  | F64Op.Div -> FloatingPoint.mk_div e1 e2 `F64Type
+  | F64Op.Mul -> FloatingPoint.mk_mul e1 e2 `F64Type
+  | F64Op.Min -> FloatingPoint.mk_min e1 e2 `F64Type
+  | F64Op.Max -> FloatingPoint.mk_max e1 e2 `F64Type
   | F64Op.CopySign -> failwith "eval F64Binop: TODO CopySign"
 
 let i32_relop op e1 e2 =
   match op with
-  | I32Op.Eq -> Relop (I32 Eq, e1, e2)
-  | I32Op.Ne -> Relop (I32 Ne, e1, e2)
-  | I32Op.LtU -> Relop (I32 LtU, e1, e2)
-  | I32Op.LtS -> Relop (I32 LtS, e1, e2)
-  | I32Op.GtU -> Relop (I32 GtU, e1, e2)
-  | I32Op.GtS -> Relop (I32 GtS, e1, e2)
-  | I32Op.LeU -> Relop (I32 LeU, e1, e2)
-  | I32Op.LeS -> Relop (I32 LeS, e1, e2)
-  | I32Op.GeU -> Relop (I32 GeU, e1, e2)
-  | I32Op.GeS -> Relop (I32 GeS, e1, e2)
+  | I32Op.Eq -> BitVector.mk_eq e1 e2 `I32Type
+  | I32Op.Ne -> BitVector.mk_ne e1 e2 `I32Type
+  | I32Op.LtU -> BitVector.mk_lt_u e1 e2 `I32Type
+  | I32Op.LtS -> BitVector.mk_lt_s e1 e2 `I32Type
+  | I32Op.GtU -> BitVector.mk_gt_u e1 e2 `I32Type
+  | I32Op.GtS -> BitVector.mk_gt_s e1 e2 `I32Type
+  | I32Op.LeU -> BitVector.mk_le_u e1 e2 `I32Type
+  | I32Op.LeS -> BitVector.mk_le_s e1 e2 `I32Type
+  | I32Op.GeU -> BitVector.mk_ge_u e1 e2 `I32Type
+  | I32Op.GeS -> BitVector.mk_ge_s e1 e2 `I32Type
 
 let i64_relop op e1 e2 =
   match op with
-  | I64Op.Eq -> Relop (I64 Eq, e1, e2)
-  | I64Op.Ne -> Relop (I64 Ne, e1, e2)
-  | I64Op.LtU -> Relop (I64 LtU, e1, e2)
-  | I64Op.LtS -> Relop (I64 LtS, e1, e2)
-  | I64Op.GtU -> Relop (I64 GtU, e1, e2)
-  | I64Op.GtS -> Relop (I64 GtS, e1, e2)
-  | I64Op.LeU -> Relop (I64 LeU, e1, e2)
-  | I64Op.LeS -> Relop (I64 LeS, e1, e2)
-  | I64Op.GeU -> Relop (I64 GeU, e1, e2)
-  | I64Op.GeS -> Relop (I64 GeS, e1, e2)
+  | I64Op.Eq -> BitVector.mk_eq e1 e2 `I64Type
+  | I64Op.Ne -> BitVector.mk_ne e1 e2 `I64Type
+  | I64Op.LtU -> BitVector.mk_lt_u e1 e2 `I64Type
+  | I64Op.LtS -> BitVector.mk_lt_s e1 e2 `I64Type
+  | I64Op.GtU -> BitVector.mk_gt_u e1 e2 `I64Type
+  | I64Op.GtS -> BitVector.mk_gt_s e1 e2 `I64Type
+  | I64Op.LeU -> BitVector.mk_le_u e1 e2 `I64Type
+  | I64Op.LeS -> BitVector.mk_le_s e1 e2 `I64Type
+  | I64Op.GeU -> BitVector.mk_ge_u e1 e2 `I64Type
+  | I64Op.GeS -> BitVector.mk_ge_s e1 e2 `I64Type
 
 let f32_relop op e1 e2 =
   match op with
-  | F32Op.Eq -> Relop (F32 Eq, e1, e2)
-  | F32Op.Ne -> Relop (F32 Ne, e1, e2)
-  | F32Op.Lt -> Relop (F32 Lt, e1, e2)
-  | F32Op.Gt -> Relop (F32 Gt, e1, e2)
-  | F32Op.Le -> Relop (F32 Le, e1, e2)
-  | F32Op.Ge -> Relop (F32 Ge, e1, e2)
+  | F32Op.Eq -> FloatingPoint.mk_eq e1 e2 `F32Type
+  | F32Op.Ne -> FloatingPoint.mk_ne e1 e2 `F32Type
+  | F32Op.Lt -> FloatingPoint.mk_lt e1 e2 `F32Type
+  | F32Op.Gt -> FloatingPoint.mk_gt e1 e2 `F32Type
+  | F32Op.Le -> FloatingPoint.mk_le e1 e2 `F32Type
+  | F32Op.Ge -> FloatingPoint.mk_ge e1 e2 `F32Type
 
 let f64_relop op e1 e2 =
   match op with
-  | F64Op.Eq -> Relop (F64 Eq, e1, e2)
-  | F64Op.Ne -> Relop (F64 Ne, e1, e2)
-  | F64Op.Lt -> Relop (F64 Lt, e1, e2)
-  | F64Op.Gt -> Relop (F64 Gt, e1, e2)
-  | F64Op.Le -> Relop (F64 Le, e1, e2)
-  | F64Op.Ge -> Relop (F64 Ge, e1, e2)
+  | F64Op.Eq -> FloatingPoint.mk_eq e1 e2 `F64Type
+  | F64Op.Ne -> FloatingPoint.mk_ne e1 e2 `F64Type
+  | F64Op.Lt -> FloatingPoint.mk_lt e1 e2 `F64Type
+  | F64Op.Gt -> FloatingPoint.mk_gt e1 e2 `F64Type
+  | F64Op.Le -> FloatingPoint.mk_le e1 e2 `F64Type
+  | F64Op.Ge -> FloatingPoint.mk_ge e1 e2 `F64Type
 
 (* TODO: sign bit *)
 let i32_cvtop op s =
